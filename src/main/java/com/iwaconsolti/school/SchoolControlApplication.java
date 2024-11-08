e92afac4f7ab39154500533bf22aa61c21384021
Francisco J Perez Gallardo <78174671+fjpgtt@users.noreply.github.com>
2024-11-08 13:23:39 -0600

    Initial commit

---
+++ src/main/java/com/iwaconsolti/school/SchoolControlApplication.java
@@ -0,0 +1,13 @@
+package com.iwaconsolti.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolControlApplication.class, args);
+	}
+
+}
